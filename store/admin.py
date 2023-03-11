069ae1e29effe0358ade71d540796c5e90cc2eee
Shivani Sirsat <48412628+Shivani180599@users.noreply.github.com>
2023-03-11 23:42:17 +0530

    Add files via upload

---
+++ store/admin.py
@@ -0,0 +1,22 @@
+# Register your models here.
+from django.contrib import admin
+#from pathlib import Path
+
+
+from .models import Category, Product #access the model in the admin 
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'slug', 'price',
+                    'in_stock', 'created', 'updated']
+    list_filter = ['in_stock', 'is_active']
+    list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug': ('title',)}
+    
